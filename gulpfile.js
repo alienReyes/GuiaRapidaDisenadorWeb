695cb9cf7839ab0bad63a636a0f9a8a22952fbde
Rodrigo Reyes <rodrigo.reyes@laureate.net>
2016-06-21 14:31:41 -0400

    background

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
+var compass = require('gulp-compass');
+
 
 // Basic Gulp task syntax
 gulp.task('hello', function() {
@@ -24,23 +26,35 @@ gulp.task('hello', function() {
 gulp.task('browserSync', function() {
   browserSync({
     server: {
-      baseDir: 'app'
+      baseDir: 'dist'
     }
   })
 })
 
-gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass()) // Passes it through a gulp-sass
-    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
-    .pipe(browserSync.reload({ // Reloading with Browser Sync
-      stream: true
-    }));
-})
+
+
+gulp.task('compass', function () {
+    gulp.src('app/scss/style.scss')
+        .pipe(compass({
+                sass: 'app/scss/',
+                css:  'app/css/',
+                image: '../images',
+                style: 'expanded',
+                require: ['susy', 'breakpoint']
+            }))
+        //.pipe(gulp.dest( outputDir + 'css'))
+        .pipe(browserSync.reload({ // Reloading with Browser Sync
+          stream: true
+        }))
+      })
+
+
+
+
 
 // Watchers
 gulp.task('watch', function() {
-  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/scss/**/*.scss', ['compass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
 })
@@ -50,7 +64,6 @@ gulp.task('watch', function() {
 
 // Optimizing CSS and JavaScript
 gulp.task('useref', function() {
-
   return gulp.src('app/*.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
@@ -89,7 +102,7 @@ gulp.task('clean:dist', function() {
 // ---------------
 
 gulp.task('default', function(callback) {
-  runSequence(['sass', 'browserSync', 'watch'],
+  runSequence(['compass','useref', 'images', 'fonts', 'browserSync','watch'],
     callback
   )
 })
@@ -97,7 +110,7 @@ gulp.task('default', function(callback) {
 gulp.task('build', function(callback) {
   runSequence(
     'clean:dist',
-    ['sass', 'useref', 'images', 'fonts'],
+    ['compass', 'useref', 'images', 'fonts'],
     callback
   )
 })
